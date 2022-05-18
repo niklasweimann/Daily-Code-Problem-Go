f7ca7acee83e5efb432a0f8a5a8690454d7263b7
Niklas Weimann <nikweimann@gmail.com>
2022-05-18 22:26:35 +0200

    Day 001

---
+++ 001/001.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	fmt.Println(getSolution(4))
+	fmt.Println(getSolution(6))
+	fmt.Println(getSolution(8))
+	fmt.Println(getSolution(10))
+}
+
+func getSolution(input int) (int, int) {
+	for i := input; i > 0; i-- {
+		var n = input - i
+		if big.NewInt(int64(i)).ProbablyPrime(0) && big.NewInt(int64(n)).ProbablyPrime(0) {
+			return n, i
+		}
+	}
+	return 0, 0
+}
